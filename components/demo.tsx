f47af3f86a8684ee87e81953fa4460b7854c6696
devgiljong <calf-love@naver.com>
2023-10-13 00:26:51 +0900

    :sparkles: 캐러셀 데모

--- components/demo.tsx
+++ components/demo.tsx
@@ -10,6 +10,18 @@ const DemoOffset = ({}: demoProps) => {
   return (
     <div>
       <div ref={ref} className=" h-[100px] w-[100px]"></div>
+      <div className=" w-full flex justify-center flex-wrap overflow-hidden">
+        <div className=" flex gap-x-10">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, idx) => (
+            <li
+              key={item}
+              className=" list-none rounded-lg flex h-[200px] w-[200px] bg-primary-100"
+            >
+              아이템
+            </li>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
